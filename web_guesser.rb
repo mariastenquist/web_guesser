67278307299af6220fe65885b847f08dbd6aea4f
mariastenquist <stenquistm@gmail.com>
2017-03-05 22:18:59 -0700

    adds color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,28 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 SECRET_NUMBER = rand(100)
-
-	def check_guess(guess)
-		if guess == SECRET_NUMBER
-			message = "You win! The SECRET NUMBER is #{SECRET_NUMBER}"
-		elsif guess.between?((SECRET_NUMBER - 5), SECRET_NUMBER)
-				message = "Guess too low!"
-		elsif guess.between?(SECRET_NUMBER, (SECRET_NUMBER + 5))
-			message = "Guess too high!"
-		elsif guess < (SECRET_NUMBER - 5)
-			message = "Guess way too low!"
-		elsif guess > (SECRET_NUMBER + 5)
-			message = "Guess way too high!"
-		else
-			"What happened?"
-		end
-	end
-
 
 	get '/' do
 	  guess = params["guess"].to_i
-	  m = check_guess(guess)
-	  erb :index, :locals => {:number => SECRET_NUMBER, :message => m}
-end
+	  message = check_guess(guess)
+
+	  erb :index, :locals => {:color => @color, :message => message}
+	end
+
+	def check_guess(guess)
+		if guess == SECRET_NUMBER
+			@color = "green"
+			message = "You win! The secret num is #{SECRET_NUMBER}"
+		elsif guess.between?((SECRET_NUMBER - 5), SECRET_NUMBER)
+				@color = 'salmon'
+				message = "Guess too low!"
+		elsif guess.between?(SECRET_NUMBER, (SECRET_NUMBER + 5))
+				@color = 'salmon'
+			message = "Guess too high!"
+		elsif guess < (SECRET_NUMBER - 5)
+				@color = 'red'
+			message = "Guess way too low!"
+		elsif guess > (SECRET_NUMBER + 5)
+				@color = 'red'
+			message = "Guess way too high!"
+		end
+	end
